c3c185dbc1838b4d5b5e71f109653a918fff7b89
api201 <api201ok10@gmail.com>
2022-03-18 12:46:28 +0300

    Rename: def test_quest_should_see_add_to_basket_button

--- Part_3_6_Step_9/test_items.py
+++ Part_3_6_Step_9/test_items.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-def test_quest_should_see_login_link(browser):
+def test_quest_should_see_add_to_basket_button(browser):
  link=f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
  browser.get(link)
  text_btn = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
